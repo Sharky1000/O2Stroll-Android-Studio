abd53800caa6f8826ad4bc61ed74b115264e8bd3
Sharky1000 <sharanyag.cd23@bmsce.ac.in>
2025-10-24 15:11:21 +0530

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,81 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.secrets.gradle.plugin)
+}
+
+android {
+    namespace = "com.example.o2stroll"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.o2stroll"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+
+    // CardView
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+
+
+    // Google Maps (for Explore feature)
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.1.0")
+
+
+    // Optional: For better date/time handling
+    implementation("joda-time:joda-time:2.12.5")
+
+    // Google Places API
+    implementation("com.google.android.libraries.places:places:3.3.0")
+    // For location permissions
+    implementation("com.google.android.gms:play-services-base:18.3.0")
+}
